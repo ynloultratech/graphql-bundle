0899e4599384e5c6209fab3dc98e4c39cd03aea9
Yonel Ceruto <yonelceruto@gmail.com>
2018-01-03 16:18:20 -0500

    Add QueryList annotation as CRUD operation

---
+++ src/Annotation/QueryList.php
@@ -0,0 +1,67 @@
+<?php
+
+/*******************************************************************************
+ *  This file is part of the GraphQL Bundle package.
+ *
+ *  (c) YnloUltratech <support@ynloultratech.com>
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ ******************************************************************************/
+
+namespace Ynlo\GraphQLBundle\Annotation;
+
+/**
+ * @Annotation()
+ *
+ * @Target({"CLASS"})
+ */
+final class QueryList
+{
+    /**
+     * @var string
+     */
+    public $name;
+
+    /**
+     * @var string
+     */
+    public $type;
+
+    /**
+     * @var string
+     */
+    public $description;
+
+    /**
+     * @var string
+     */
+    public $deprecationReason;
+
+    /**
+     * @var \Ynlo\GraphQLBundle\Annotation\Argument[]
+     */
+    public $arguments = [];
+
+    /**
+     * @var string
+     */
+    public $resolver;
+
+    /**
+     * Options used by extensions
+     *
+     * @var array
+     */
+    public $options = [];
+
+    /**
+     * @var string[]
+     */
+    public $searchFields = [];
+
+    /**
+     * @var int
+     */
+    public $limit;
+}
