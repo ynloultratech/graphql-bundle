6e1fdd5f160bae4985194fea879a11498e1fb0e6
Yonel Ceruto <yonelceruto@gmail.com>
2018-01-09 15:38:33 -0500

    revert controller file header changes

--- src/Controller/GraphQLEndpointController.php
+++ src/Controller/GraphQLEndpointController.php
@@ -1,5 +1,13 @@
 <?php
-/*introspection*/
+
+/*******************************************************************************
+ *  This file is part of the GraphQL Bundle package.
+ *
+ *  (c) YnloUltratech <support@ynloultratech.com>
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ ******************************************************************************/
 
 namespace Ynlo\GraphQLBundle\Controller;
 
