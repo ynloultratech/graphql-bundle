b98576dda55d19c28f96abbe1de978d202965302
Yonel Ceruto <yonelceruto@gmail.com>
2018-01-18 13:19:15 -0500

    Add roles definition extension to secure schema introspection

---
+++ src/Definition/Extension/RolesDefinitionExtension.php
@@ -0,0 +1,68 @@
+<?php
+
+/*******************************************************************************
+ *  This file is part of the GraphQL Bundle package.
+ *
+ *  (c) YnloUltratech <support@ynloultratech.com>
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ ******************************************************************************/
+
+namespace Ynlo\GraphQLBundle\Definition\Extension;
+
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Ynlo\GraphQLBundle\Definition\ExecutableDefinitionInterface;
+use Ynlo\GraphQLBundle\Definition\Registry\Endpoint;
+
+class RolesDefinitionExtension extends AbstractDefinitionExtension
+{
+    private $authorizationChecker;
+
+    public function __construct(AuthorizationCheckerInterface $authorizationChecker)
+    {
+        $this->authorizationChecker = $authorizationChecker;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function buildConfig(ArrayNodeDefinition $root): void
+    {
+        $root
+            ->info('List of roles for queries and mutations')
+            ->prototype('scalar')
+            ->end();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function configureEndpoint(Endpoint $endpoint): void
+    {
+        $endpoint->setQueries($this->secureDefinitions($endpoint->allQueries(), $endpoint));
+    }
+
+    /**
+     * @param ExecutableDefinitionInterface[] $definitions
+     * @param Endpoint                        $endpoint
+     *
+     * @return ExecutableDefinitionInterface[]
+     */
+    private function secureDefinitions(array $definitions, Endpoint $endpoint): array
+    {
+        $secureDefinitions = [];
+        foreach ($definitions as $definition) {
+            if ($roles = $definition->getRoles()) {
+                if ($this->authorizationChecker->isGranted($roles)) {
+                    $secureDefinitions[] = $definition;
+                }
+            } else {
+                $secureDefinitions[] = $definition;
+            }
+        }
+
+        return $secureDefinitions;
+    }
+}
