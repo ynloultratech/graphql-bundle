23a69c81271a9ff2414f3b778b734ae5fc713e4b
Rafael Santos <raf.rsr@gmail.com>
2018-01-23 10:40:37 -0500

    Export schema in json format like introspection query

--- src/Controller/SchemaController.php
+++ src/Controller/SchemaController.php
@@ -10,7 +10,10 @@
 
 namespace Ynlo\GraphQLBundle\Controller;
 
+use GraphQL\GraphQL;
 use GraphQL\Utils\SchemaPrinter;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Ynlo\GraphQLBundle\Schema\SchemaCompiler;
 
@@ -18,14 +21,115 @@ class SchemaController
 {
     private $compiler;
 
+    private const INTROSPECTION_QUERY = <<<GraphQL
+query IntrospectionQuery {
+    __schema {
+      queryType { name }
+      mutationType { name }
+      subscriptionType { name }
+      types {
+        ...FullType
+      }
+      directives {
+        name
+        description
+        locations
+        args {
+          ...InputValue
+        }
+      }
+    }
+  }
+
+  fragment FullType on __Type {
+    kind
+    name
+    description
+    fields(includeDeprecated: true) {
+      name
+      description
+      args {
+        ...InputValue
+      }
+      type {
+        ...TypeRef
+      }
+      isDeprecated
+      deprecationReason
+    }
+    inputFields {
+      ...InputValue
+    }
+    interfaces {
+      ...TypeRef
+    }
+    enumValues(includeDeprecated: true) {
+      name
+      description
+      isDeprecated
+      deprecationReason
+    }
+    possibleTypes {
+      ...TypeRef
+    }
+  }
+
+  fragment InputValue on __InputValue {
+    name
+    description
+    type { ...TypeRef }
+    defaultValue
+  }
+
+  fragment TypeRef on __Type {
+    kind
+    name
+    ofType {
+      kind
+      name
+      ofType {
+        kind
+        name
+        ofType {
+          kind
+          name
+          ofType {
+            kind
+            name
+            ofType {
+              kind
+              name
+              ofType {
+                kind
+                name
+                ofType {
+                  kind
+                  name
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+GraphQL;
+
+
     public function __construct(SchemaCompiler $compiler)
     {
         $this->compiler = $compiler;
     }
 
-    public function __invoke(): Response
+    public function __invoke(Request $request): Response
     {
         $schema = $this->compiler->compile();
+
+        if ('json' === $request->getRequestFormat()) {
+            $result = GraphQL::executeQuery($schema, self::INTROSPECTION_QUERY);
+
+            return JsonResponse::create($result->toArray());
+        }
 
         return new Response(SchemaPrinter::doPrint($schema));
     }
