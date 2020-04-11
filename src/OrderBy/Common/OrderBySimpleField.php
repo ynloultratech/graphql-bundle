cb616fb864e04349f468ef4c11368c8283c853f1
Rafael Santos <raf.rsr@gmail.com>
2020-04-10 22:50:17 -0400

    fix error using alias in orderBy

--- src/OrderBy/Common/OrderBySimpleField.php
+++ src/OrderBy/Common/OrderBySimpleField.php
@@ -14,6 +14,7 @@ use Doctrine\ORM\QueryBuilder;
 use Ynlo\GraphQLBundle\Model\OrderBy;
 use Ynlo\GraphQLBundle\OrderBy\OrderByContext;
 use Ynlo\GraphQLBundle\OrderBy\OrderByInterface;
+use Ynlo\GraphQLBundle\Util\FieldOptionsHelper;
 
 class OrderBySimpleField implements OrderByInterface
 {
@@ -23,6 +24,13 @@ class OrderBySimpleField implements OrderByInterface
     public function __invoke(OrderByContext $context, QueryBuilder $qb, $alias, OrderBy $orderBy)
     {
         $column = $orderBy->getField();
+
+        // use alias
+        $orderByFields = FieldOptionsHelper::normalize($context->getParentContext()->getDefinition()->getMeta('pagination')['order_by'] ?? ['*']);
+        if (isset($orderByFields[$column])){
+            $column = $orderByFields[$column];
+        }
+
         $qb->addOrderBy("{$alias}.$column", $orderBy->getDirection());
     }
 }
