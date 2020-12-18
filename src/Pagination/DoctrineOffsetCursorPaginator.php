df672087f341ad7170f62ccd9f4a61139291edca
Rafael Santos <raf.rsr@gmail.com>
2020-12-18 16:08:13 -0500

    remove groupBy part during list pagination

--- src/Pagination/DoctrineOffsetCursorPaginator.php
+++ src/Pagination/DoctrineOffsetCursorPaginator.php
@@ -104,6 +104,10 @@ class DoctrineOffsetCursorPaginator implements DoctrineCursorPaginatorInterface
             $countQuery->resetDQLPart('orderBy');
         }
 
+        if ($countQuery->getDQLPart('groupBy')) {
+            $countQuery->resetDQLPart('groupBy');
+        }
+
         $countQuery->setMaxResults(null);
         $countQuery->setFirstResult(0);
 
