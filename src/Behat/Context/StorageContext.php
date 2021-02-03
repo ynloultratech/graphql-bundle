868145cdd782c707c26b30af9cb93c9c5e46c4cd
Rafael Santos <raf.rsr@gmail.com>
2021-02-03 16:35:13 -0500

    add method to debug grabbed variables

--- src/Behat/Context/StorageContext.php
+++ src/Behat/Context/StorageContext.php
@@ -36,4 +36,20 @@ final class StorageContext implements Context, StorageAwareInterface
     {
         $this->storage->setValue($name, $value);
     }
+
+    /**
+     * Print helpful debug information for settled variables
+     *
+     * @Then debug grabbed variables
+     */
+    public function debugGrabbedVariables()
+    {
+        print_r("\n\n");
+        print_r("\033[46m------------------- VARIABLES-----------------------\033[0m\n\n");
+        $variables = $this->storage->getData() ?? null;
+        print_r(json_encode($variables, JSON_PRETTY_PRINT));
+        print_r("\n\n");
+        print_r("-----------------------------------------------------------------\n\n");
+        ob_flush();
+    }
 }
