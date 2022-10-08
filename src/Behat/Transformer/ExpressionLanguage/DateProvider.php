5a3ba87170ca43e05caf2062e926db2d6ba02207
Rafael Santos <raf.rsr@gmail.com>
2022-10-08 15:01:55 -0400

    [behat] add `date()` provider

---
+++ src/Behat/Transformer/ExpressionLanguage/DateProvider.php
@@ -0,0 +1,41 @@
+<?php
+/*******************************************************************************
+ *  This file is part of the GraphQL Bundle package.
+ *
+ *  (c) YnloUltratech <support@ynloultratech.com>
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ ******************************************************************************/
+
+namespace Ynlo\GraphQLBundle\Behat\Transformer\ExpressionLanguage;
+
+use Symfony\Component\ExpressionLanguage\ExpressionLanguage;
+
+/**
+ * Add support  to use Date Function
+ *
+ * @example "{date('2010-01-01')}" => "DateTime instace for given date"
+ * @example "{date()}" => current DateTime instance
+ */
+class DateProvider implements ExpressionPreprocessorInterface
+{
+    public function setUp(ExpressionLanguage $el, string &$expression, array &$values)
+    {
+        if (preg_match('/date\(/', $expression)) {
+            $el->register(
+                'date',
+                function ($expression) {
+                    return sprintf('$date(%s)', $expression);
+                },
+                function (array $variables, $expression) {
+                    return $variables['date']($expression);
+                }
+            );
+
+            $values['date'] = function ($expression) {
+                return new \DateTime($expression);
+            };
+        }
+    }
+}
